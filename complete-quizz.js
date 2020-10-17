ddc2d2768c08663da4bdf7618ea8e62fdeb643a7
Oleksandr Khotemskyi <xotabu4@gmail.com>
2020-10-17 17:54:52 +0300

    Update complete-quizz.js

--- complete-quizz.js
+++ complete-quizz.js
@@ -1,48 +1,59 @@
 const puppeteer = require("puppeteer");
-const { data } = require("./data.js");
+const { data } = require("./data_real.js");
+
+console.log('Working with results length', data.length);
 
 (async () => {
     let browser;
-    try {
-        console.time("Quizz completion took");
-        browser = await puppeteer.launch({
-            // headless: false,
-            defaultViewport: {
-                width: 1280,
-                height: 800
-            }
-        });
-        const page = await browser.newPage();
-        await page.goto("https://psytests.org/psystate/san1.html");
+    for (let setOfAnswers of data) {
+        try {
+            console.log(`Running for ${setOfAnswers["Email address"]}`)
+            browser = await puppeteer.launch({
+                headless: false,
+                defaultViewport: {
+                    width: 1280,
+                    height: 800
+                }
+            });
+            const page = await browser.newPage();
+            await page.goto("https://psytests.org/psystate/san1.html");
 
-        await page.waitFor(500);
-        console.log("###ID", browser.process().pid);
-        await page.evaluate(arg1 => {
-            answers = JSON.parse(arg1);
-            console.log("###", answers);
+            await page.waitFor(500);
+            // console.log("###ID", browser.process().pid);
+            await page.evaluate(arg1 => {
+                for ([question, answer] of Object.entries(JSON.parse(arg1))) {
+                    console.log(question)
+                    console.log(answer)
+                    if (question === "Email address" ||
+                        question === "Name" ||
+                        question === "Timestamp"
+                    ) {
+                        console.log('skipping', question)
+                        continue;
+                    }
+                    try {
+                        qtlAnswer(parseInt(question, 10), parseInt(answer, 10));
+                    } catch (err) {
+                        // no-op
+                    }
+                }
+            }, JSON.stringify(setOfAnswers));
+            await page.waitFor(500);
+            await page.waitFor("input.stdResButton");
+            await (await page.$("input.stdResButton")).click();
 
-            for ([question, answer] of Object.entries(answers)) {
-                console.log(question, answer);
-                try {
-                    qtlAnswer(question, answer);
-                } catch (err) {}
-            }
-        }, JSON.stringify(data[0].answers));
-        await page.waitFor(500);
-        await page.waitFor("input.stdResButton");
-        await (await page.$("input.stdResButton")).click();
-
-        const shareResult = 'a[title=" постоянная ссылка на ваш результат "]';
-        await page.waitFor(shareResult);
-        await page.waitFor(500);
-        const href = await page.$eval(shareResult, e => e.getAttribute("href"));
-        console.log("$$$", href);
-        await (await page.$("body")).screenshot({
-            path: `screenshots/${browser.process().pid}.png`
-        });
-        await browser.close();
-        console.timeEnd("Quizz completion took");
-    } catch (err) {
-        browser.close().catch(err => {});
+            const shareResult = 'a[title=" постоянная ссылка на ваш результат "]';
+            await page.waitFor(shareResult);
+            await page.waitFor(500);
+            const href = await page.$eval(shareResult, e => e.getAttribute("href"));
+            console.log(`${setOfAnswers["Email address"]}: `, href);
+            await (await page.$("body")).screenshot({
+                path: `screenshots/${setOfAnswers["Email address"]}.png`
+            });
+            await browser.close();
+        } catch (err) {
+            console.log(err)
+            browser.close().catch(err => { });
+        }
     }
 })();
